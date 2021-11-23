c5725b9f4e990ce8b2a8cc2f383f1f9d5b88e0d9
y00njae <rladbswo12@gist.ac.kr>
2021-11-23 22:06:46 +0900

    1.13 DB 정보 접근 및 장고 템플릿 내 for loop

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,9 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+from django.urls import reverse
+
+
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD'})
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        # hello_world_list = HelloWorld.objects.all()
+        # render 는 post request 를 계속 유지하는 느낌이고 redirect 는 get 요청으로 바꾸는 느낌
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD !'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
