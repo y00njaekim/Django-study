4e7609ae2d2af30e379230fda3b1539a3dca3cb8
y00njae <rladbswo12@gist.ac.kr>
2021-11-29 01:58:31 +0900

    2-5. UpdateView를 이용한 비밀번호 변경 구현

--- pinterest/accountapp/urls.py
+++ pinterest/accountapp/urls.py
@@ -1,9 +1,7 @@
-from accountapp.views import AccountCreateView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView
 from accountapp.views import hello_world
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
-from accountapp.views import AccountDetailView
 
 app_name = "accountapp"
 
@@ -13,4 +11,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 ]
