4a397273166ece7493b02d1143e5fe8ebac946e6
y00njae <rladbswo12@gist.ac.kr>
2021-11-29 22:41:33 +0900

    3-2. Decorator를 이용한 코드 간소화

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 
 <img src="https://user-images.githubusercontent.com/56385667/142998697-21f1d51e-845c-4a63-ba4f-645bb7325f17.png" style="zoom: 15%"/>
 
+### Decorator를 이용한 코드 간소화
+
+<img src="https://user-images.githubusercontent.com/56385667/143874026-b0810a5f-fd14-4975-911d-36fbe25a14f5.png" />
+
+<img src="https://user-images.githubusercontent.com/56385667/143874035-fb3b81d6-eb4b-4c0f-8193-a5d3d803082c.png" />
+
--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,32 +1,34 @@
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 # Create your views here.
 from accountapp.models import HelloWorld
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+has_ownership = [account_ownership_required, login_required]
 
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            # hello_world_list = HelloWorld.objects.all()
-            # render 는 post request 를 계속 유지하는 느낌이고 redirect 는 get 요청으로 바꾸는 느낌
-            # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # hello_world_list = HelloWorld.objects.all()
+        # render 는 post request 를 계속 유지하는 느낌이고 redirect 는 get 요청으로 바꾸는 느낌
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
     model = User
@@ -34,23 +36,13 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -58,22 +50,10 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
-
-
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
